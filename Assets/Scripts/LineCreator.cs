4450a9402067ed82a903a54ab1730214df150832
Jeff Polasz <jeff_polasz@hotmail.com>
2018-02-03 00:46:13 -0500

    Added Android functionality

--- Assets/Scripts/LineCreator.cs
+++ Assets/Scripts/LineCreator.cs
@@ -17,31 +17,61 @@ public class LineCreator : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			mouseDown = true;
-		}
+		// Android specific code
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.touchCount > 0) {
+				// Gets only the first touch (No multi-touch)
+				if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+					line.positionCount = vertexCount + 1;
+					Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+					line.SetPosition (vertexCount, mousePos);
+					vertexCount++;
 
-		if (mouseDown) {
-			line.positionCount = vertexCount + 1;
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			line.SetPosition (vertexCount, mousePos);
-			vertexCount++;
+					BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
+					box.transform.position = line.transform.position;
+					box.size = new Vector2 (0.1f, 0.1f);
+				}
 
-			BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
-			box.transform.position = line.transform.position;
-			box.size = new Vector2 (0.1f, 0.1f);
-		}
+				if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+					line.positionCount = 0;
+					vertexCount = 0;
 
-		if (Input.GetMouseButtonUp (0)) {
-			mouseDown = false;
-			line.positionCount = 0;
-			vertexCount = 0;
+					BoxCollider2D[] colliders = GetComponents<BoxCollider2D> ();
+					foreach (BoxCollider2D box in colliders) {
+						Destroy (box);
+					}
+				}
+			}
 
-			BoxCollider2D[] colliders = GetComponents<BoxCollider2D> ();
-			foreach (BoxCollider2D box in colliders) {
-				Destroy (box);
+		// Windows specific code (Editor)
+		} else if (Application.platform == RuntimePlatform.WindowsEditor) {
+
+			if (Input.GetMouseButtonDown (0)) {
+				mouseDown = true;
+			}
+
+			if (mouseDown) {
+				line.positionCount = vertexCount + 1;
+				Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				line.SetPosition (vertexCount, mousePos);
+				vertexCount++;
+
+				BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
+				box.transform.position = line.transform.position;
+				box.size = new Vector2 (0.1f, 0.1f);
+			}
+
+			if (Input.GetMouseButtonUp (0)) {
+				mouseDown = false;
+				line.positionCount = 0;
+				vertexCount = 0;
+
+				BoxCollider2D[] colliders = GetComponents<BoxCollider2D> ();
+				foreach (BoxCollider2D box in colliders) {
+					Destroy (box);
+				}
 			}
 		}
-	}
+	} // Update
 
 } // LineCreator
