c118a7dddfa481d5b7b1d84e6e653da18957b6d3
Jeff Polasz <jeff_polasz@hotmail.com>
2018-02-03 00:28:11 -0500

    Added swipe effect

---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+	int vertexCount = 0;
+	bool mouseDown = false;
+	LineRenderer line;
+
+	void Awake () {
+		line = GetComponent<LineRenderer> ();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			mouseDown = true;
+		}
+
+		if (mouseDown) {
+			line.positionCount = vertexCount + 1;
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			line.SetPosition (vertexCount, mousePos);
+			vertexCount++;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			mouseDown = false;
+			line.positionCount = 0;
+			vertexCount = 0;
+		}
+	}
+
+} // LineCreator
