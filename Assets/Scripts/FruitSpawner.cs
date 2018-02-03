b7bbe2c5570321f82f48930a9552a0c6abd296de
Jeff Polasz <jeff_polasz@hotmail.com>
2018-02-03 01:06:30 -0500

    Added FruitSpawner and random rotation

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+	public GameObject fruit;
+	public float maxX;
+
+	void Start () {
+		Invoke ("StartSpawning", 1f);
+	}
+
+	void Update () {
+		
+	}
+
+	public void StartSpawning () {
+		InvokeRepeating ("SpawnFruitGroups", 1f, 6f);
+	}
+
+	public void StopSpawning () {
+		CancelInvoke ("SpawnFruitGroup");
+		StopCoroutine ("SpawnFruit");
+	}
+
+	public void SpawnFruitGroups () {
+		StartCoroutine ("SpawnFruit");
+	}
+
+	IEnumerator SpawnFruit () {
+		for (int i = 0; i < 5; i++) {
+			float rand = Random.Range (-maxX, maxX);
+			Vector3 pos = new Vector3 (rand, transform.position.y, 0);
+			GameObject f = Instantiate (fruit, pos, Quaternion.identity) as GameObject;
+			f.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 15f), ForceMode2D.Impulse);
+			f.GetComponent<Rigidbody2D> ().AddTorque (Random.Range (-20f, 20f));
+
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+} // FruitSpawner
