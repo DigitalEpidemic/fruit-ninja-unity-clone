2d93a9cb287b7805791935b977306bd065c9bcd2
Jeff Polasz <jeff_polasz@hotmail.com>
2018-02-04 01:06:35 -0500

    Added MainMenu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void PlayGame () {
+		SceneManager.LoadScene ("Level1");
+	}
+
+} // MainMenuController
